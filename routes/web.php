72a2d415fda7b173177dd5e838bb85ed2a18d30a
Muhammad Rifqy <2ndrifqy@gmail.com>
2024-04-30 13:25:31 +0700

    boilerplate

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,13 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::redirect('/', '/login');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
 });
 
-require __DIR__.'/auth.php';
+
+require __DIR__ . '/auth.php';
