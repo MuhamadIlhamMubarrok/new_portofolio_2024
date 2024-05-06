5de680273f1f94fd19d13c3a4ea6c061ab06f3f1
Muhammad Rifqy <2ndrifqy@gmail.com>
2024-05-07 00:04:16 +0700

    hero

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -1,6 +1,6 @@
 <nav class="sidebar">
     <div class="sidebar-header">
-        <a href="{{ route('dashboard.index') }}">
+        <a href="{{ route('admin.dashboard.index') }}">
             <img src="#" alt="KKRA">
         </a>
         <div class="sidebar-toggler not-active">
@@ -13,10 +13,28 @@
         <ul class="nav">
             <li class="nav-item nav-category">Main</li>
             <li class="nav-item">
-                <a href="{{ route('dashboard.index') }}" class="nav-link">
+                <a href="{{ route('admin.dashboard.index') }}" class="nav-link">
                     <i class="link-icon" data-feather="box"></i>
                     <span class="link-title">Dashboard</span>
                 </a>
+            </li>
+            <li class="nav-item {{ request()->routeIs('admin.dashboard.hero.*') ? 'active' : '' }}">
+                <a class="nav-link" data-bs-toggle="collapse" href="#pages" role="button" aria-expanded="false"
+                    aria-controls="pages">
+                    <i class="link-icon" data-feather="grid"></i>
+                    <span class="link-title">Pages</span>
+                    <i class="link-arrow" data-feather="chevron-down"></i>
+                </a>
+                <div class="collapse {{ request()->routeIs('admin.dashboard.hero.*') ? 'show' : '' }}" id="pages">
+                    <ul class="nav sub-menu">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link {{ request()->routeIs('admin.dashboard.hero.*') ? 'active' : '' }}">Hero</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Banner</a>
+                        </li>
+                    </ul>
+                </div>
             </li>
         </ul>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,20 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\HeroController;
 use App\Http\Controllers\Admin\DashboardController;
 
-Route::redirect('/', '/login');
+Route::get('/', function () {
+    return redirect('/login');
+});
 
-Route::middleware('auth')->group(function () {
-    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+Route::name('admin.')->prefix('admin')->group(function () {
+    Route::middleware('auth')->group(function () {
+        Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+        Route::get('dashboard/hero', [HeroController::class, 'hero'])->name('dashboard.hero');
+        Route::put('dashboard/hero/update/{id}', [HeroController::class, 'hero'])->name('dashboard.hero.update');
+    });
 });
 
 
