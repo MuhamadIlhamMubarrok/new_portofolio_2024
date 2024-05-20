61434ed8c4b708cba244d053996939d294fd4454
Muhammad Rifqy <2ndrifqy@gmail.com>
2024-05-20 18:56:21 +0700

    kontak

--- resources/views/pages/admin/profile/index.blade.php
+++ resources/views/pages/admin/profile/index.blade.php
@@ -4,17 +4,11 @@
 
 @section('content')
 
-    @push('style')
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Dropify/0.2.2/css/dropify.min.css"
-            integrity="sha512-EZSUkJWTjzDlspOoPSpUFR0o0Xy7jdzW//6qhUkoZ9c4StFkVsp9fbbd0O06p9ELS3H486m4wmrCELjza4JEog=="
-            crossorigin="anonymous" referrerpolicy="no-referrer" />
-    @endpush
-
     <div class="page-content">
 
         <nav class="page-breadcrumb">
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="/">Dashboard</a></li>
+                <li class="breadcrumb-item"><a href="{{ route('admin.dashboard.index') }}">Dashboard</a></li>
                 <li class="breadcrumb-item active" aria-current="page">profile</li>
             </ol>
         </nav>
@@ -60,12 +54,3 @@
         </div>
     </div>
 @endsection
-
-@push('script')
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Dropify/0.2.2/js/dropify.min.js"
-        integrity="sha512-8QFTrG0oeOiyWo/VM9Y8kgxdlCryqhIxVeRpWSezdRRAvarxVtwLnGroJgnVW9/XBRduxO/z1GblzPrMQoeuew=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script>
-        $('.dropify').dropify();
-    </script>
-@endpush
--- resources/views/pages/admin/sumber-data/index.blade.php
+++ resources/views/pages/admin/sumber-data/index.blade.php
@@ -4,12 +4,6 @@
 
 @section('content')
 
-    @push('style')
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Dropify/0.2.2/css/dropify.min.css"
-            integrity="sha512-EZSUkJWTjzDlspOoPSpUFR0o0Xy7jdzW//6qhUkoZ9c4StFkVsp9fbbd0O06p9ELS3H486m4wmrCELjza4JEog=="
-            crossorigin="anonymous" referrerpolicy="no-referrer" />
-    @endpush
-
     <div class="page-content">
 
         <nav class="page-breadcrumb">
@@ -65,12 +59,3 @@
         </div>
     </div>
 @endsection
-
-@push('script')
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Dropify/0.2.2/js/dropify.min.js"
-        integrity="sha512-8QFTrG0oeOiyWo/VM9Y8kgxdlCryqhIxVeRpWSezdRRAvarxVtwLnGroJgnVW9/XBRduxO/z1GblzPrMQoeuew=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script>
-        $('.dropify').dropify();
-    </script>
-@endpush
